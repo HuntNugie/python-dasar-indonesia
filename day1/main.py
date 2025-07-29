a67c2adafde4743076de59d061f3527729ffe46e
Nugie kurniawan <nugiebinhar@gmail.com>
2025-07-29 22:31:36 +0700

    Belajar pemograman python dasar

---
+++ day1/main.py
@@ -0,0 +1,9 @@
+
+# mengeprint output di python
+print("Hallo dunia nama saya adalah Nugie kurniawan")
+
+# ini adalah komentar
+
+# cara untuk compile python(biasa nya lebih cepat)
+# menggunakan library py_compile
+# cara nya : python3 -m py_compile 
