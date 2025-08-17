aa4e4a7521ad8b8c9a1a013a6e630fe6b734d640
Nugie kurniawan <nugiebinhar@gmail.com>
2025-08-17 22:12:11 +0700

    latihan/komparasi dan logika

---
+++ latihan/komparasiDanLogika/main.py
@@ -0,0 +1,10 @@
+# kurang dari 3 atau lebih dari 10
+print("Kurang dari 3 atau lebih dari 10")
+user = int(input("Masukan angka anda : "))
+hasil = (user<3) or (user>10)
+
+print("Hasil dari Kurang dari 3 atau lebih dari 10 adalah ",hasil)
+print("lebih dari 3 dan kurang dari 10")
+hasil = (user>3) and (user<10)
+print(f"Hasil dari lebih dari 3 dan kurang dari 10 adalah {hasil}")
+
